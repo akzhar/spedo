eed2f381ae7b3800ea49a94f1646ce01d23f7e11
Akzhar <akzharik@mail.ru>
2019-02-14 01:11:46 +0300

    add goods

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,13 +1,15 @@
 var slides = document.querySelectorAll(".slide__item"); //все слайды
-var radio = document.querySelectorAll(".slide__radio"); //все радикнопки
+var label = document.querySelectorAll(".slide__radiolabel"); //все лейблы радикнопок
 var currentSlide = 0; //счетчик слайдов
 var next = document.querySelector('.slider__btn-next'); //кнопка вперед
 var previous = document.querySelector('.slider__btn-prev'); // кнопка назад
-var slideInterval = setInterval(nextSlide,3000); //интервал повторения
+var slideInterval = setInterval(nextSlide,5000); //интервал повторения
 
 function pauseSlideshow() {
   clearInterval(slideInterval); //очистка интервала повторения
 };
+
+label[0].style.backgroundColor="transparent";
 
 next.onclick = function() { //при клике на вперед
   pauseSlideshow(); //прекращение авто слайд шоу
@@ -19,8 +21,8 @@ previous.onclick = function() { // при клике на назад
   previousSlide(); //вызов функции
 };
 
-for (var i = 0; i < radio.length; i ++) {
-  radio[i].onclick = function() {
+for (var i = 0; i < label.length; i ++) {
+  label[i].onclick = function() {
     pauseSlideshow(); //прекращение авто слайд шоу
     goToSlide(this.id); //переход к слайду (по id input'а)
   };
@@ -35,9 +37,9 @@ function previousSlide() {
 };
 
 function goToSlide(n) {
-  radio[currentSlide].removeAttribute("checked"); //снятие атрибута у текущей радио радио кнопки
+  label[currentSlide].style.backgroundColor=""; //снятие атрибута у текущей радио радио кнопки
   slides[currentSlide].classList.remove("slide__item--show"); //убираем класс с текущего слайда
   currentSlide = (n+slides.length)%slides.length; // текущий слайд -1 или +1
-  radio[currentSlide].setAttribute("checked", true); //установка атрибута у текущей радио радио кнопки
+  label[currentSlide].style.backgroundColor="transparent"; //установка атрибута у текущей радио радио кнопки
   slides[currentSlide].classList.add("slide__item--show"); //назначаем класс текущему слайду
 };
