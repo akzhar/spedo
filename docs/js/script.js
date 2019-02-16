944074552153ac36a729c090c17eae8aad74560b
Akzhar <akzharik@mail.ru>
2019-02-16 21:03:49 +0300

    Safari img --> object

--- docs/js/script.js
+++ docs/js/script.js
@@ -49,10 +49,10 @@ var selectLanguage = document.getElementById("selectLanguage");
 var selectCurrency = document.getElementById("selectCurrency");
 
 selectLanguage.addEventListener("change", function(){
-  document.getElementById("iconLanguage").setAttribute("src","img/"+this.value+".svg");
+  document.getElementById("iconLanguage").setAttribute("data","img/"+this.value+".svg");
 });
 
 selectCurrency.addEventListener("change", function(){
-  document.getElementById("iconCurrency").setAttribute("src","img/"+this.value+".svg");
+  document.getElementById("iconCurrency").setAttribute("data","img/"+this.value+".svg");
 });
 
--- src/js/icons.js
+++ src/js/icons.js
@@ -2,9 +2,9 @@ var selectLanguage = document.getElementById("selectLanguage");
 var selectCurrency = document.getElementById("selectCurrency");
 
 selectLanguage.addEventListener("change", function(){
-  document.getElementById("iconLanguage").setAttribute("src","img/"+this.value+".svg");
+  document.getElementById("iconLanguage").setAttribute("data","img/"+this.value+".svg");
 });
 
 selectCurrency.addEventListener("change", function(){
-  document.getElementById("iconCurrency").setAttribute("src","img/"+this.value+".svg");
+  document.getElementById("iconCurrency").setAttribute("data","img/"+this.value+".svg");
 });
